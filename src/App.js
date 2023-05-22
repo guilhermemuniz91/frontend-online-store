0d38eba37c36ff3a668f013299e9c55da1af85a0
Mateus Garcia <mateus.nazareth@hotmail.com>
2023-05-22 17:32:16 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import ShoppingCart from './components/ShoppingCart';
 // import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route path="/" component={ Home } />
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/ShoppingCart" component={ ShoppingCart } />
       </Switch>
     </div>
   );
