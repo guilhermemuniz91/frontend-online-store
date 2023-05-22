4f966fc898546cc861ea4bd04e38796791a9ef6b
devNattan <natanaelhenrique.s.c@gmail.com>
2023-05-22 16:44:36 -0300

    finalizando requisito 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
 // import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
 function App() {
   return (
-    <div />
+    <div>
+      <Switch>
+        <Route path="/" component={ Home } />
+      </Switch>
+    </div>
   );
 }
 
