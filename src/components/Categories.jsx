5abfa1ef799eb487edf669b31f4577c6ee136b1d
luishrs <luis-hrs@outlook.com>
2023-05-23 16:39:20 -0300

    Feat: Adicionado lógica que renderiza produtos por categorias

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,6 +4,7 @@ import { getCategories } from '../services/api';
 export default class Categories extends Component {
   state = {
     productCategories: [],
+    resultsCategories: [],
   };
 
   componentDidMount() {
@@ -12,14 +13,19 @@ export default class Categories extends Component {
 
   setCategories = async () => {
     const response = await getCategories();
-    this.setState({
-      productCategories: response,
-    });
+    this.setState((prev) => ({ ...prev, productCategories: response }));
+  };
+
+  productsByCategories = async (id) => {
+    const data = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${id}`);
+    const response = await data.json();
+    console.log(response);
+    this.setState((prev) => ({ ...prev, resultsCategories: response.results }));
   };
 
   render() {
-    const { productCategories } = this.state;
-    console.log(productCategories);
+    const { productCategories, resultsCategories } = this.state;
+
     return (
       <div>
         {
@@ -28,11 +34,22 @@ export default class Categories extends Component {
               data-testid="category"
               type="button"
               key={ categori.id }
+              onClick={ () => this.productsByCategories(categori.id) }
             >
               {categori.name}
-
             </button>))
         }
+        <div>
+          {
+            resultsCategories.map((product) => (
+              <div key={ product.id } data-testid="product">
+                <p>{product.title}</p>
+                <img src={ product.thumbnail } alt={ product.title } />
+                <p>{product.price}</p>
+              </div>
+            ))
+          }
+        </div>
 
       </div>
     );
